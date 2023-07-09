f05ab006b6e483b05777cc0e0e21bf79de708787
GaeunU <65946325+gaeunU@users.noreply.github.com>
2023-07-09 14:28:43 +0900

    project_setting

---
+++ C_Study/Source/C_Study/C_StudyGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "C_StudyGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class C_STUDY_API AC_StudyGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
